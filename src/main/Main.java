deeeab106620bb27f8d09d88f54d5912f25e6a06
Alexis Sessarego <Alexis.joel.sessarego@gmail.com>
2017-09-01 22:43:18 -0300

    Testing comment

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
  * @param args
  */
 	public static void main(String[] args) {
-
+		// testing comment
 		String file="in";
 		Vendedoras Vendedoras = new Vendedoras();
 		Vendedora vendor;
